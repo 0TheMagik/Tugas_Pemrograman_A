3d6995358798500dd4a3e4d96971b9b04aa8697e
Izzan Nawa Syarif <izzan.nsyarif@gmail.com>
2025-05-08 20:50:24 +0700

    add interpolation

--- Program.c
+++ Program.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-void interpolasi(x, y, n, xi, yint, ea){
-    
+
+void NewtonInterpolation(double x[], double y[], int n, double xi, double *yint, double ea[]) {
+    double fdd[n][n];
+
+    for (int i = 0; i < n; i++) {
+        fdd[i][0] = y[i];
+    }
+
+    for (int j = 1; j < n; j++) {
+        for (int i = 0; i < n - j; i++) {
+            fdd[i][j] = (fdd[i+1][j-1] - fdd[i][j-1]) / (x[i+j] - x[i]);
+        }
+    }
+
+    double xterm = 1.0;
+    yint[0] = fdd[0][0];
+
+    for (int order = 1; order < n; order++) {
+        xterm *= (xi - x[order - 1]);
+        yint[order] = yint[order - 1] + fdd[0][order] * xterm;
+        ea[order - 1] = yint[order] - yint[order - 1];
+    }
 }
